08dffe2cf281e7e888a46d0279975e4a5df62611
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-08-07 20:26:08 -0600

    CLASES

---
+++ act13.py
@@ -0,0 +1,18 @@
+class Repartidor():
+    def __init__(self, nombre, paquetes, zona):
+        self.nombre = nombre
+        self.paquetes = paquetes
+        self.zona = zona
+        def Mostrar(self):
+            print(f"Nombre: {self.nombre} Paquetes: {self.paquetes} en zona: {self.zona}x")
+
+
+class Mensajeria():
+    def __init__(self, repartidor):
+        self.repartidor = repartidor
+
+
+
+
+
+
